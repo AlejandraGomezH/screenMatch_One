9358873914f4f672ce5aa74c263cc6f91913f77f
AlejandraGomezH <93231057+AlejandraGomezH@users.noreply.github.com>
2024-11-07 22:27:16 -0600

    ADD polimorfismo, herencia y paquetes

--- src/Principal.java
+++ src/Principal.java
@@ -1,20 +1,49 @@
+import com.aluracursos.screenmatch.calculos.CalculadoraDeTiempo;
 import com.aluracursos.screenmatch.modelos.Pelicula;
+import com.aluracursos.screenmatch.modelos.Serie;
 
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula(); //com.aluracursos.screenmatch.modelos.Pelicula: es un tipo de dato, despues le damos el nombre a la variable, nuevo e instanciamos pelicula() Que significa una instacncia, el compilador crea un espacio de memoria para diferentes atributos.
         miPelicula.setNombre("Encanto");
         miPelicula.setFechaLanzamiento(2021);
-        miPelicula.setDuracionEnminutos(120);
+        miPelicula.setDuracionEnMinutos(120);
         miPelicula.setIncluidoEnElPlan(true);
 
         miPelicula.muestraFichaTecnica();
         miPelicula.evalua(10);
         miPelicula.evalua(10);
         miPelicula.evalua(10);
-        System.out.println(miPelicula.getTotalEvaluaciones());
-        System.out.println(miPelicula.calcularMedia());
-        System.out.println(miPelicula.getNombre());
+        System.out.println("El total de evaluaciones es: " + miPelicula.getTotalEvaluaciones());
+        System.out.println("La media de las evaluaciones es:" + miPelicula.calcularMedia());
+        System.out.println("El nombre de la pelicula es: " + miPelicula.getNombre());
+
+        Serie serie = new Serie();
+
+        serie.setNombre("DEXTER");
+        serie.setTemporadas(12);
+        serie.setEpisodiosPorTemporada(5);
+        serie.setMinutosPorEpisodio(12);
+        serie.muestraFichaTecnica();
+        System.out.println("La serie dura: " + serie.getDuracionEnMinutos());
+
+
+        Pelicula peliculaDos = new Pelicula();
+        peliculaDos.setNombre("VENOM");
+        peliculaDos.setFechaLanzamiento(2024);
+        peliculaDos.setDuracionEnMinutos(120);
+
+
+        CalculadoraDeTiempo calculadora = new CalculadoraDeTiempo();
+
+        calculadora.incluye(miPelicula);
+        calculadora.incluye(serie);
+        calculadora.incluye(peliculaDos);
+        System.out.println("Necesitas " + calculadora.getTiempoTotal() + " minutos para ver tus series");
+
+
+
+
 
 
         //otraPelicula.muestraFichaTecnica();
---
+++ src/com/aluracursos/screenmatch/modelos/Titulo.java
@@ -0,0 +1,62 @@
+package com.aluracursos.screenmatch.modelos;
+
+public class Titulo {
+    private String nombre;
+    private int fechaLanzamiento;
+    private int duracionEnMinutos;
+    boolean incluidoEnElPlan;
+    private double sumaDeLasEvaluaciones;
+    private int totalEvaluaciones;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public int getFechaLanzamiento() {
+        return fechaLanzamiento;
+    }
+
+    public int getDuracionEnMinutos() {
+        return duracionEnMinutos;
+    }
+
+    public boolean isIncluidoEnElPlan() {
+        return incluidoEnElPlan;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre; // this indica que es el atributo de nuestra clase el que declaramos arriba.
+    }
+
+    public void setFechaLanzamiento(int fechaLanzamiento) {
+        this.fechaLanzamiento = fechaLanzamiento;
+    }
+
+    public void setDuracionEnMinutos(int duracionEnMinutos) {
+        this.duracionEnMinutos = duracionEnMinutos;
+    }
+
+    public void setIncluidoEnElPlan(boolean incluidoEnElPlan) {
+        this.incluidoEnElPlan = incluidoEnElPlan;
+    }
+
+    public int getTotalEvaluaciones(){
+        return totalEvaluaciones;
+    }
+
+    public void muestraFichaTecnica(){ //void no nos retorna nada, se usa el modo imperativo en el verbo de la función.
+        System.out.println("El nombre de la película es "+ nombre);
+        System.out.println("El lanzamiento es: "+  fechaLanzamiento);
+        System.out.println("Duración en minutos : "+  getDuracionEnMinutos());
+    }
+
+    public void evalua(double nota){
+        sumaDeLasEvaluaciones += nota;
+        totalEvaluaciones ++;
+    }
+
+    public double calcularMedia(){//ponemos double porque nos va a retornar un tipo double.
+
+        return sumaDeLasEvaluaciones / totalEvaluaciones;
+    }
+}
